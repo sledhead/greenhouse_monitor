77c367422cad69a0db9f9fd9479d140686a1d923
Sled Man <testme@gmail.com>
2018-05-01 16:45:43 -0600

    play sound

--- test_load.py
+++ test_load.py
@@ -4,6 +4,7 @@ import urllib2
 import json
 import re
 import time
+from playsound import playsound
 
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
@@ -51,3 +52,34 @@ html = response.read()
 response.close()
 
 print html
+
+json_result = json.loads(html)
+
+print json_result["row0"][0]["gh_ntemp"]
+#print type(json_result["row0"][0]["gh_ntemp"])
+
+#test the json result, see if the temp values are outside of margins
+if( float(json_result["row0"][0]["gh_ntemp"]) <  48 ):
+	#Below the defined limit
+	#Send error msg
+	print 'Hit temp. level'
+	temp_str = 'North Green House section is below defined limit\n'
+	temp_str += 'North Green House Temp:' + str(json_result["row0"][0]["gh_ntemp"])
+	temp_str += '\nSouth Green House Temp:' + str(json_result["row0"][0]["gh_stemp"])
+	temp_str += '\nControl Green House Temp:' + str(json_result["row0"][0]["con_temp"])
+	temp_str += '\nOutside Green House Temp:' + str(json_result["row0"][0]["out_temp"])
+	SendEmail(temp_str,'858 temp problem')
+	
+if( float(json_result["row0"][0]["gh_ntemp"]) >  90 ):
+	#Above the defined limit
+	#Send error msg
+	print 'Hit temp. level'
+	temp_str = 'North Green House section is above defined limit\n'
+	temp_str += 'North Green House Temp:' + str(json_result["row0"][0]["gh_ntemp"])
+	temp_str += '\nSouth Green House Temp:' + str(json_result["row0"][0]["gh_stemp"])
+	temp_str += '\nControl Green House Temp:' + str(json_result["row0"][0]["con_temp"])
+	temp_str += '\nOutside Green House Temp:' + str(json_result["row0"][0]["out_temp"])
+	SendEmail(temp_str,'858 temp problem')
+	
+	#play sound bit
+	playsound('sounds/sound1.mp3')
